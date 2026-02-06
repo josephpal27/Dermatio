dbac368ec867a48e7ad86009d04ec1906717c778
Joseph Pal <josephpal27@gmail.com>
2026-02-06 12:05:19 +0530

    Lenis Smooth Scroll added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+
   useEffect(() => {
     const lenis = new Lenis({
       smooth: true,
       lerp: 0.08,
     });
-
-    // Sync Lenis with GSAP ScrollTrigger
-    lenis.on("scroll", ScrollTrigger.update);
 
     function raf(time) {
       lenis.raf(time);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,10 @@
-import { useEffect } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
-  useEffect(() => {
-    const sections = gsap.utils.toArray(".stack-section");
-
-    sections.forEach((section, index) => {
-      const nextSection = sections[index + 1];
-      if (!nextSection) return;
-
-      ScrollTrigger.create({
-        trigger: section,
-        start: "top top",
-        endTrigger: nextSection,
-        end: "top top",
-        pin: true,
-        pinSpacing: false,
-        scrub: true,
-      });
-    });
-
-    return () => ScrollTrigger.getAll().forEach(t => t.kill());
-  }, []);
-
   return (
     <>
-      <section className="stack-section bg-one">
-        <div className="content">
-          <h1>Section One</h1>
-        </div>
-      </section>
 
-      <section className="stack-section bg-two">
-        <div className="content">
-          <h1>Section Two</h1>
-        </div>
-      </section>
-
-      <section className="stack-section bg-three">
-        <div className="content">
-          <h1>Section Three</h1>
-        </div>
-      </section>
-
-      <section className="stack-section bg-four">
-        <div className="content">
-          <h1>Section Four</h1>
-        </div>
-      </section>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
