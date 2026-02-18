1fc3cc47b8a1df16bbcbf10da47b91a03b1a2095
Joseph Pal <josephpal27@gmail.com>
2026-02-18 13:55:25 +0530

    Highlights Responsiveness added

--- client/src/components/HighLights.jsx
+++ client/src/components/HighLights.jsx
@@ -33,9 +33,9 @@ const HighLights = () => {
                 flex
                 justify-between
                 flex-wrap
-                py-[3rem]
+                py-[3rem] sm:py-[2rem] lg:py-[2.2rem] xl:py-[2.7rem] 2xl:py-[3rem]
                 px-[7%]
-                mt-[1rem]
+                mt-[1rem] sm:mt-[1rem] lg:mt-[0.6rem] xl:mt-[0.8rem] 2xl:mt-[1rem]
             ">
                 {
                     highLightsData.map((item, index) => {
@@ -50,15 +50,15 @@ const HighLights = () => {
                                     src={item.image} 
                                     alt={item.title}
                                     className="
-                                        w-[60px]
-                                        h-[60px]
+                                        w-[60px] sm:w-[50px] lg:w-[50px] xl:w-[55px] 2xl:w-[60px]
+                                        h-[60px] sm:h-[50px] lg:h-[50px] xl:h-[55px] 2xl:h-[60px]
                                         object-contain
                                     "
                                 />
                                 <span dangerouslySetInnerHTML={{ __html: item.title }} className="
                                     text-[#363636]
-                                    text-[1.6rem]
-                                    mt-[1.5rem]
+                                    text-[1.6rem] sm:text-[1.6rem] lg:text-[1.3rem] xl:text-[1.5rem] 2xl:text-[1.6rem]
+                                    mt-[1.5rem] sm:mt-[1.5rem] lg:mt-[1.1rem] xl:mt-[1.3rem] 2xl:mt-[1.5rem]
                                     text-center
                                     leading-[1.3]
                                 "/>
