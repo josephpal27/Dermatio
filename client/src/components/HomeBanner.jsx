7dc58d6a8d8eec2a3f887f136c69c3585e39a359
Joseph Pal <josephpal27@gmail.com>
2026-02-18 13:47:03 +0530

    Home Banner Responsiveness added

--- client/src/components/HomeBanner.jsx
+++ client/src/components/HomeBanner.jsx
@@ -12,39 +12,33 @@ const HomeBanner = () => {
                     alt="Home Banner"
                     loading="eager"
                     className="
-                        w-full
-                        rounded-b-[45px]
+                        w-full object-cover
+                        rounded-b-[45px] sm:rounded-b-[40px] lg:rounded-b-[35px] xl:rounded-b-[40px] 2xl:rounded-b-[45px]
                         h-[93vh]
-                        object-cover
                 "/>
                 {/* Layer */}
                 <div className="
                     absolute
-                    top-0
-                    left-0
-                    w-full
-                    h-full
+                    top-0 left-0 w-full h-full text-[#fff]
                     flex
                     justify-between
                     px-[7%]
-                    pb-[8rem]
-                    text-[#fff]
+                    pb-[8rem] sm:pb-[6rem] lg:pb-[6rem] xl:pb-[7rem] 2xl:pb-[8rem]
                 ">
                     {/* Left */}
                     <div className="
                         w-[40%]
-                        flex
-                        flex-col
+                        flex flex-col
                         justify-end
                     ">
                         <span className="
-                            text-[1.2rem]
+                            text-[1.1rem] sm:text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
                         ">
                             #1st product to <br /> fight 4 signs of eczema
                         </span>
                         <h1 className="
-                            text-[4rem]
-                            mt-[1rem]
+                            text-[1.4rem] sm:text-[1rem] lg:text-[3.2rem] xl:text-[3.6rem] 2xl:text-[4rem]
+                            mt-[1rem] sm:mt-[1rem] lg:mt-[0.6rem] xl:mt-[0.8rem] 2xl:mt-[1rem]
                             leading-[1]
                         ">
                             Gentle Care <br /> Proven Science
@@ -53,27 +47,26 @@ const HomeBanner = () => {
                     {/* Right */}
                     <div className="
                         w-[34%]
-                        flex
-                        flex-col
+                        flex flex-col
                         justify-center
                     ">
                         <span className="
-                            text-[2rem]
+                            text-[1.5rem] sm:text-[2rem] lg:text-[1.6rem] xl:text-[1.8rem] 2xl:text-[2rem]
                             leading-[1.2]
-                            mt-[10rem]
+                            mt-[10rem] sm:mt-[8rem] lg:mt-[8rem] xl:mt-[9rem] 2xl:mt-[10rem]
                         ">
                             Eczema relief for <br /> every stage of Childhood
                         </span>
                         <Link to="/" className="
-                            mt-[1.5rem]
-                            border-[2px]
+                            mt-[1.5rem] sm:mt-[1rem] lg:mt-[1.1rem] xl:mt-[1.3rem] 2xl:mt-[1.5rem]
+                            border-[2px] sm:border-[1px] lg:border-[1.3px] xl:border-[1.5px] 2xl:border-[2px]
                             border-white
                             w-max
-                            py-[0.4rem]
-                            pl-[1rem]
-                            pr-[0.5rem]
+                            py-[0.4rem] sm:py-[0.4rem] lg:py-[0.3rem] xl:py-[0.35rem] 2xl:py-[0.4rem]
+                            pl-[1rem] sm:pl-[1rem] lg:pl-[0.9rem] xl:pl-[0.95rem] 2xl:pl-[1rem]
+                            pr-[0.5rem] sm:pr-[0.5rem] lg:pr-[0.4rem] xl:pr-[0.45rem] 2xl:pr-[0.5rem]
                             rounded-[30px]
-                            text-[0.95rem]
+                            text-[0.9rem] sm:text-[0.9rem] lg:text-[0.8rem] xl:text-[0.88rem] 2xl:text-[0.95rem]
                             transition
                             hover:bg-white
                             hover:text-black
@@ -87,11 +80,15 @@ const HomeBanner = () => {
                                 alt="Arrow" 
                                 loading="lazy"
                                 className=" 
-                                    w-[23px]
-                                    h-[23px]
-                                    ml-[0.8rem]
+                                    w-[20px] sm:w-[20px] lg:w-[19px] xl:w-[21px] 2xl:w-[23px]
+                                    h-[20px] sm:h-[20px] lg:h-[19px] xl:h-[21px] 2xl:h-[23px]
+                                    ml-[0.8rem] sm:ml-[0.8rem] lg:ml-[0.6rem] xl:ml-[0.7rem] 2xl:ml-[0.8rem]
                                     duration-200
-                                    group-hover:ml-[1.2rem]
+                                    group-hover:2xl:ml-[1.2rem]
+                                    group-hover:xl:ml-[1.1rem]
+                                    group-hover:lg:ml-[1rem]
+                                    group-hover:sm:ml-[0.9rem]
+                                    group-hover:ml-[1rem]
                             "/>
                         </Link>
                     </div>
