651bbf28b064f244ebdf2d44ba7c50399d609d4a
Joseph Pal <josephpal27@gmail.com>
2026-03-19 13:04:59 +0530

    Product Gallery Mobile View Added

--- client/src/components/product-details/ProductDetailsBanner.jsx
+++ client/src/components/product-details/ProductDetailsBanner.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 
 const ProductDetailsBanner = () => {
     return (
@@ -10,23 +9,20 @@ const ProductDetailsBanner = () => {
                     src="/images/banners/product-details-banner.avif" 
                     alt="Product Details Banner" 
                     loading="eager"
-                    className="w-full"
+                    className="
+                        w-full h-[110px] sm:h-auto
+                        rounded-b-[15px] sm:rounded-b-[20px] lg:rounded-b-[20px] xl:rounded-b-[25px] 2xl:rounded-b-[30px]
+                    "
                 />
                 <div className="
                     absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-[#fff]
                     px-[1rem] sm:px-[5%] lg:px-[7%]
                 ">
                     <h1 className="
-                        text-[1.8rem] sm:text-[1rem] lg:text-[3.2rem] xl:text-[3.6rem] 2xl:text-[4rem]
+                        text-[2rem] sm:text-[1rem] lg:text-[3.2rem] xl:text-[3.6rem] 2xl:text-[4rem]
                     " data-aos="fade-up">
                         Shop
                     </h1>
-                    <p className="
-                        mt-[0.7rem] sm:mt-[0.5rem] lg:mt-[0.5rem] xl:mt-[0.6rem] 2xl:mt-[0.7rem]
-                        text-[0.9rem] sm:text-[0.9rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.9rem]
-                    " data-aos="fade">
-                        <Link to="/" className="hover:underline">Home</Link> / Shop
-                    </p>
                 </div>
             </section>
         </>
--- client/src/components/product-details/ProductGallery.jsx
+++ client/src/components/product-details/ProductGallery.jsx
@@ -18,22 +18,22 @@ const ProductGallery = () => {
     return (
         <>
             <section className="
-                px-[7%]
-                py-[4rem] sm:py-[2.5rem] lg:py-[3.3rem] xl:py-[3.6rem] 2xl:py-[4rem]
+                px-[1rem] sm:px-[5%] lg:px-[7%]
+                py-[2rem] sm:py-[2.5rem] lg:py-[3.3rem] xl:py-[3.6rem] 2xl:py-[4rem]
                 flex justify-between flex-wrap
             ">
 
                 {/* Product Gallery */}
                 <div className="
-                    w-[50%]
+                    w-[100%] sm:w-[50%]
                 ">
                     <Tab.Container defaultActiveKey="0">
-                        <div className="flex items-center">
+                        <div className="flex flex-wrap items-center">
                             {/* Side Thumbnails */}
                             <Nav className="
                                 flex flex-col 
                                 gap-[0.8rem] sm:gap-[0.8rem] lg:gap-[0.6rem] xl:gap-[0.7rem] 2xl:gap-[0.8rem]
-                                w-[17%]
+                                w-[20%] sm:w-[17%]
                             ">
                                 {product.galleryImages.map((img, index) => (
                                     <Nav.Item key={index}>
@@ -45,8 +45,8 @@ const ProductGallery = () => {
                                                     w-full
                                                     aspect-square object-cover bg-[#eee7dd]
                                                     rounded-[5px]
-                                                    px-[7px]
-                                                    py-[10px]
+                                                    px-[2px] sm:px-[7px]
+                                                    py-[7px] sm:py-[10px]
                                         "/>
                                         </Nav.Link>
                                     </Nav.Item>
@@ -55,8 +55,8 @@ const ProductGallery = () => {
 
                             {/* Main Image */}
                             <Tab.Content className="
-                                w-[80%] 
-                                pt-[1rem] sm:pt-[1rem] lg:pt-[0.8rem] xl:pt-[0.9rem] 2xl:pt-[1rem]
+                                w-[80%]
+                                pt-[0.5rem] sm:pt-[1rem] lg:pt-[0.8rem] xl:pt-[0.9rem] 2xl:pt-[1rem]
                             ">
                                 {product.galleryImages.map((img, index) => (
                                     <Tab.Pane key={index} eventKey={index}>
@@ -74,24 +74,25 @@ const ProductGallery = () => {
 
                 {/* Product Details */}
                 <div className="
-                    w-[47%]
+                    w-[100%] sm:w-[47%]
+                    mt-[2rem] sm:mt-0
                 ">
                     <span className="
-                        text-[1.1rem] sm:text-[1.1rem] lg:text-[0.85rem] xl:text-[1rem] 2xl:text-[1.1rem]
+                        text-[1rem] sm:text-[1.1rem] lg:text-[0.85rem] xl:text-[1rem] 2xl:text-[1.1rem]
                         tracking-[1px]
                         text-[#becb0c] font-[600]
                     ">
                         {product.tag}
                     </span>
                     <h2 className="
-                        text-[2.5rem] sm:text-[2rem] lg:text-[1.9rem] xl:text-[2.2rem] 2xl:text-[2.5rem]
+                        text-[1.7rem] sm:text-[2rem] lg:text-[1.9rem] xl:text-[2.2rem] 2xl:text-[2.5rem]
                         mt-[0.5rem] sm:mt-[0.5rem] lg:mt-[0.3rem] xl:mt-[0.4rem] 2xl:mt-[0.5rem]
                         text-[#131313]
                     ">
                         {product.name}
                     </h2>
                     <span className="
-                        text-[2rem] sm:text-[2rem] lg:text-[1.5rem] xl:text-[1.75rem] 2xl:text-[2rem]
+                        text-[1.4rem] sm:text-[2rem] lg:text-[1.5rem] xl:text-[1.75rem] 2xl:text-[2rem]
                         uppercase font-[600]
                     ">
                         {product.type}
@@ -99,12 +100,12 @@ const ProductGallery = () => {
 
                     {/* Description */}
                     <div className="
-                        mt-[1rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
+                        mt-[0.8rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
                     ">
                         {product.desc.intro.map((text, i) => (
                             <p key={i} className="
-                                text-[0.9rem] sm:text-[0.9rem] lg:text-[0.75rem] xl:text-[0.85rem] 2xl:text-[0.95rem]
-                                mb-[0.3rem]
+                                text-[1rem] sm:text-[0.9rem] lg:text-[0.75rem] xl:text-[0.85rem] 2xl:text-[0.95rem]
+                                mb-[0.5rem] sm:mb-[0.3rem]
                                 text-[#4b4b4b]
                             ">
                                 {text}
@@ -112,10 +113,10 @@ const ProductGallery = () => {
                         ))}
 
                         <ul className="
-                            mt-[0.5rem] sm:mt-[0.5rem] lg:mt-[0.3rem] xl:mt-[0.4rem] 2xl:mt-[0.5rem]
+                            mt-[0.7rem] sm:mt-[0.5rem] lg:mt-[0.3rem] xl:mt-[0.4rem] 2xl:mt-[0.5rem]
                             list-disc 
                             pl-[1rem] sm:pl-[1rem] lg:pl-[0.8rem] xl:pl-[0.9rem] 2xl:pl-[1rem]
-                            text-[0.9rem] sm:text-[0.9rem] lg:text-[0.75rem] xl:text-[0.85rem] 2xl:text-[0.95rem]
+                            text-[1rem] sm:text-[0.9rem] lg:text-[0.75rem] xl:text-[0.85rem] 2xl:text-[0.95rem]
                             text-[#4b4b4b]
                         ">
                             {product.desc.points.map((point, i) => (
@@ -125,7 +126,7 @@ const ProductGallery = () => {
                     </div>
 
                     <span className="
-                        text-[2.2rem] sm:text-[2rem] lg:text-[1.6rem] xl:text-[1.9rem] 2xl:text-[2.2rem]
+                        text-[2.1rem] sm:text-[2rem] lg:text-[1.6rem] xl:text-[1.9rem] 2xl:text-[2.2rem]
                         mt-[1rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
                         font-[600] text-[#131313] block
                     ">
@@ -134,11 +135,11 @@ const ProductGallery = () => {
 
                     {/* Size */}
                     <div className="
-                        mt-[0.5rem]
+                        mt-[0.8rem] sm:mt-[0.5rem]
                         flex items-center
                     ">
                         <span className="
-                            text-[1rem] sm:text-[1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]
+                            text-[1.1rem] sm:text-[1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]
                         ">
                             Size :
                         </span>
@@ -148,10 +149,10 @@ const ProductGallery = () => {
                                 onClick={() => setSelectedSize(item)}
                                 className={`
                                     px-[1rem] sm:px-[1rem] lg:px-[0.8rem] xl:px-[0.9rem] 2xl:px-[1rem]
-                                    pt-[0.4rem] sm:pt-[0.4rem] lg:pt-[0.3rem] xl:pt-[0.38rem] 2xl:pt-[0.4rem]
-                                    pb-[0.2rem] sm:pb-[0.2rem] lg:pb-[0.26rem] xl:pb-[0.24rem] 2xl:pb-[0.25rem]
+                                    pt-[0.35rem] sm:pt-[0.4rem] lg:pt-[0.3rem] xl:pt-[0.38rem] 2xl:pt-[0.4rem]
+                                    pb-[0.25rem] sm:pb-[0.2rem] lg:pb-[0.26rem] xl:pb-[0.24rem] 2xl:pb-[0.25rem]
                                     rounded-full
-                                    text-[0.8rem] sm:text-[0.8rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.85rem]
+                                    text-[1rem] sm:text-[0.8rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.85rem]
                                     ml-[1rem] sm:ml-[1rem] lg:ml-[0.8rem] xl:ml-[0.9rem] 2xl:ml-[1rem]
                                     transition
                                     ${selectedSize.size === item.size
@@ -166,16 +167,16 @@ const ProductGallery = () => {
                     </div>
 
                     <div className="
-                        mt-[2.5rem] sm:mt-[2rem] lg:mt-[1.9rem] xl:mt-[2.2rem] 2xl:mt-[2.5rem]
-                        flex items-center 
-                        gap-[1rem] sm:gap-[1rem] lg:gap-[0.8rem] xl:gap-[0.9rem] 2xl:gap-[1rem]
+                        mt-[2rem] sm:mt-[2rem] lg:mt-[1.9rem] xl:mt-[2.2rem] 2xl:mt-[2.5rem]
+                        flex items-center justify-between sm:justify-normal
+                        gap-[0] sm:gap-[1rem] lg:gap-[0.8rem] xl:gap-[0.9rem] 2xl:gap-[1rem]
                     ">
                         <button className="
-                            bg-[#005aed] hover:bg-[#004ecb] text-[#fff] transition
-                            px-[1.8rem] sm:px-[1.8rem] lg:px-[1.6rem] xl:px-[1.7rem] 2xl:px-[1.8rem]
-                            py-[0.55rem] sm:py-[0.5rem] lg:py-[0.45rem] xl:py-[0.5rem] 2xl:py-[0.55rem]
-                            rounded-[10px]
-                            text-[1.3rem] sm:text-[1rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
+                            bg-[#005aed] hover:bg-[#004ecb] text-[#fff] transition w-[50%] sm:w-auto
+                            px-[1rem] sm:px-[1.8rem] lg:px-[1.6rem] xl:px-[1.7rem] 2xl:px-[1.8rem]
+                            py-[0.6rem] sm:py-[0.5rem] lg:py-[0.45rem] xl:py-[0.5rem] 2xl:py-[0.55rem]
+                            rounded-[10px] text-center
+                            text-[1.2rem] sm:text-[1rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
                         " onClick={() => {
                                 addToCart(product, selectedSize);
                                 navigate("/cart");
@@ -183,11 +184,11 @@ const ProductGallery = () => {
                             Add to Cart
                         </button>
                         <Link to="/checkout" className="
-                            bg-[#becb0c] hover:bg-[#aeba05] text-[#fff] transition
-                            px-[1.8rem] sm:px-[1.8rem] lg:px-[1.6rem] xl:px-[1.7rem] 2xl:px-[1.8rem]
-                            py-[0.55rem] sm:py-[0.5rem] lg:py-[0.45rem] xl:py-[0.5rem] 2xl:py-[0.55rem]
-                            rounded-[10px]
-                            text-[1.3rem] sm:text-[1rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
+                            bg-[#becb0c] hover:bg-[#aeba05] text-[#fff] transition w-[46%] sm:w-auto
+                            px-[1rem] sm:px-[1.8rem] lg:px-[1.6rem] xl:px-[1.7rem] 2xl:px-[1.8rem]
+                            py-[0.6rem] sm:py-[0.5rem] lg:py-[0.45rem] xl:py-[0.5rem] 2xl:py-[0.55rem]
+                            rounded-[10px] text-center
+                            text-[1.2rem] sm:text-[1rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
                         ">
                             Buy Now
                         </Link>
@@ -198,12 +199,12 @@ const ProductGallery = () => {
 
             {/* Product Description */}
             <section className="
-                px-[7%]
+                px-[1rem] sm:px-[5%] lg:px-[7%]
             ">
                 <div className="
                     bg-[#fbf4ea]
-                    p-[1.5rem] sm:p-[1.2rem] lg:p-[1.1rem] xl:p-[1.3rem] 2xl:p-[1.5rem]
-                    rounded-[5px]
+                    p-[1rem] sm:p-[1.2rem] lg:p-[1.1rem] xl:p-[1.3rem] 2xl:p-[1.5rem]
+                    rounded-[8px] sm:rounded-[5px]
                     text-[1.1rem] sm:text-[1rem] lg:text-[0.85rem] xl:text-[0.98rem] 2xl:text-[1.04rem]
                 ">
                     <p>
